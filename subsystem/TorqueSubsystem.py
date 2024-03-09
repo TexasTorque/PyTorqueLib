0c61ed712f79a05491a00785f12f2f6410ef90d2
humandavey <davey.adams.three@gmail.com>
2024-03-09 10:21:16 -0600

    Update subsystems

---
+++ subsystem/TorqueSubsystem.py
@@ -0,0 +1,22 @@
+class Subsystem:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.subsystems: list[Subsystem] = []
+
+    def initialize(self) -> None:
+        print(self.name + " has not overridden the initialize function!")
+
+    def update(self) -> None:
+        print(self.name + " has not overridden the update function!")
+    
+    def get_name(self) -> str:
+        return self.name
+    
+    def set_subsystems(self, subsystems: list[Subsystem]) -> None:
+        self.subsystems = subsystems
+
+    def get_subsystem(self, name: str) -> Subsystem | None:
+        for subsystem in self.subsystems:
+            if subsystem.get_name() == name:
+                return subsystem
