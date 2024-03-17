dfa0fe697083d0d72ae00d2fe4651cc3ade18468
humandavey <davey.adams.three@gmail.com>
2024-03-17 13:04:15 -0500

    Update robotbase

--- robot/TorqueRobot.py
+++ robot/TorqueRobot.py
@@ -4,6 +4,7 @@ import systems
 class TorqueRobotBase(wpilib.TimedRobot):
 
     def __init__(self) -> None:
+        super().__init__()
         systems.add_subsystems()
     
     def robotInit(self) -> None:
