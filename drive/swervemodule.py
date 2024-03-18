84577a82d865fa58d1c3e79eaf668afd5845ba42
humandavey <davey.adams.three@gmail.com>
2024-03-18 17:07:25 -0500

    Finish swervemodule

--- drive/swervemodule.py
+++ drive/swervemodule.py
@@ -1,9 +1,10 @@
 import math
-import wpimath.kinematics
-import wpimath.geometry
-import wpimath.controller
-import phoenix6.hardware.cancoder
-import motor.neo
+import wpilib
+from wpimath.controller import PIDController, SimpleMotorFeedforwardMeters
+from phoenix6.hardware.cancoder import CANcoder
+from motor.neo import Neo
+from wpimath.kinematics import SwerveModulePosition, SwerveModuleState
+from wpimath.geometry import Rotation2d
 
 kWheelRadius = 0.0508
 kEncoderResolution = 4096
@@ -13,29 +14,60 @@ kModuleMaxAngularAcceleration = math.tau
 class SwerveModule:
     def __init__(self, name: str, driveID: int, turnID: int, encoderID: int) -> None:
         self.name = name
-        self.drive = motor.neo.Neo(driveID)
+        self.drive = Neo(driveID)
         self.drive.set_current_limit(35)
         self.drive.set_voltage_compensation(12.6)
         self.drive.set_break_mode(True)
-        # self.drive.set_conversion_factor()
+        self.drive.set_conversion_factor(.0485823156, 8.097052603e-4)
         self.drive.burn_flash()
 
-        self.turn = motor.neo.Neo(turnID)
+        self.turn = Neo(turnID)
         self.turn.set_current_limit(25)
         self.turn.set_voltage_compensation(12.6)
         self.turn.set_break_mode(True)
-        # self.turn.set_conversion_factor()
+        self.turn.set_conversion_factor(77.97432966, 1)
         self.turn.burn_flash()
 
-        self.encoder: phoenix6.hardware.cancoder.CANcoder = phoenix6.hardware.cancoder.CANcoder(encoderID)
+        self.encoder: CANcoder = CANcoder(encoderID)
 
-        self.drivePID = wpimath.controller.PIDController(.1, 0, 0)
-        self.turnPID = wpimath.controller.PIDController(.5, 0, 0)
+        self.drivePID = PIDController(.1, 0, 0)
+        self.turnPID = PIDController(.5, 0, 0)
         self.turnPID.enableContinuousInput(-180, 180)
+        self.driveFF = SimpleMotorFeedforwardMeters(.1, .2)
+
+        self.aggregate_position = SwerveModulePosition(0, Rotation2d.fromDegrees(0))
+        self.last_sampled_time = -1
     
-    def set_desired_state(self, state: wpimath.kinematics.SwerveModuleState, use_smart_drive: bool) -> None:
-        optimized = wpimath.kinematics.SwerveModuleState.optimize(
-            state,
-            wpimath.geometry.Rotation2d.fromRotations(self.encoder.get_absolute_position().value_as_double
-        ))
+    def set_desired_state(self, state: SwerveModuleState, use_smart_drive: bool) -> None:
+        optimized = SwerveModuleState.optimize(state, self.get_rotation())
         
+        if use_smart_drive:
+            drivePIDOutput = self.drivePID.calculate(self.drive.get_velocity(), optimized.speed)
+            driveFFOutput = self.driveFF.calculate(optimized.speed)
+
+            self.drive.set_percent(drivePIDOutput + driveFFOutput)
+        else:
+            self.drive.set_percent(optimized.speed / 4.6)
+        
+        turnPIDOutput = -self.turnPID.calculate(self.encoder.get_absolute_position().value_as_double)
+        self.turn.set_percent(turnPIDOutput)
+
+        if not wpilib.RobotBase.isReal():
+            time = wpilib.Timer.getFPGATimestamp()
+            if self.last_sampled_time == -1:
+                self.last_sampled_time = time
+            delta_time = time - self.last_sampled_time
+            self.last_sampled_time = time
+            self.aggregate_position.distance += optimized.speed * delta_time
+            self.aggregate_position.angle = optimized.angle
+    
+    def get_state(self) -> SwerveModuleState:
+        return SwerveModuleState(self.drive.get_velocity(), self.get_rotation())
+    
+    def get_position(self) -> SwerveModulePosition:
+        if wpilib.RobotBase.isReal():
+            return self.aggregate_position
+        return SwerveModulePosition(self.drive.get_position(), self.get_rotation())
+    
+    def get_rotation(self) -> Rotation2d:
+        return Rotation2d.fromRotations(self.encoder.get_absolute_position().value_as_double)
