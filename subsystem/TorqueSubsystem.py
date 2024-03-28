98a270ef18b51f334919aaf4c0ca1e54708a746c
humandavey <davey.adams.three@gmail.com>
2024-03-28 15:42:44 -0500

    Implement abstraction in subsystems

--- subsystem/TorqueSubsystem.py
+++ subsystem/TorqueSubsystem.py
@@ -1,8 +1,11 @@
 from lib.base.TorqueMode import TorqueMode
+from abc import ABC, abstractmethod
 
-class TorqueSubsystem:
+class TorqueSubsystem(ABC):
+    @abstractmethod
     def initialize(self, mode: TorqueMode) -> None:
         print("Must override the update function!")
 
+    @abstractmethod
     def update(self, mode: TorqueMode) -> None:
         print("Must override the update function!")
