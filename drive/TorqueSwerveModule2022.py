1c3bc9eda574af2c6e932a2b94bf87db791b94b9
humandavey <davey.adams.three@gmail.com>
2024-03-29 11:43:05 -0500

    Add swerveconfig class

--- drive/TorqueSwerveModule2022.py
+++ drive/TorqueSwerveModule2022.py
@@ -15,31 +15,62 @@ class SwervePorts:
         self.turnID = turnID
         self.encoderID = encoderID
 
+class SwerveConfig:
+    def __init__(self, free_wheel_speed: float = 4.6, drive_gear_ratio: float = 6.75, turn_gear_ratio: float = 13.71,
+                 drive_p: float = .1, drive_i: float = 0, drive_d: float = 0, drive_ff: float = .2, drive_max_current: float = 35,
+                 turn_p: float = .5, turn_i: float = 0, turn_d: float = 0, turn_max_current: float = 25,
+                 voltage_compensation: float = 12.6, max_acceleration: float = 3, max_angular_velocity: float = math.pi,
+                 max_angular_acceleration: float = math.pi, wheel_diameter: float = 4 * .0254
+                 ) -> None:
+        self.free_wheel_speed = free_wheel_speed
+        self.max_velocity = free_wheel_speed
+        self.drive_gear_ratio = drive_gear_ratio
+        self.turn_gear_ratio = turn_gear_ratio
+        self.drive_p = drive_p
+        self.drive_i = drive_i
+        self.drive_d = drive_d
+        self.drive_ff = drive_ff
+        self.drive_max_current = drive_max_current
+        self.turn_max_current = turn_max_current
+        self.turn_p = turn_p
+        self.turn_i = turn_i
+        self.turn_d = turn_d
+        self.voltage_compensation = voltage_compensation
+        self.max_acceleration = max_acceleration
+        self.max_angular_velocity = max_angular_velocity
+        self.max_angular_acceleration = max_angular_acceleration
+        self.wheel_diameter = wheel_diameter
+
+        self.drive_velocity_factor = (1 / drive_gear_ratio / 60) * (wheel_diameter * math.pi)
+        self.drive_pose_factor = (1 / drive_gear_ratio) * (wheel_diameter * math.pi) 
+
 class TorqueSwerveModule2022:
-    def __init__(self, name: str, ports: SwervePorts, offset: float = 0) -> None:
+    def __init__(self, name: str, ports: SwervePorts, offset: float = 0, config: SwerveConfig = SwerveConfig()) -> None:
         self.name = name
+        self.config = config
+
         self.drive = TorqueNEO(ports.driveID)
-        self.drive.set_current_limit(35)
-        self.drive.set_voltage_compensation(12.6)
+        self.drive.set_current_limit(config.drive_max_current)
+        self.drive.set_voltage_compensation(config.voltage_compensation)
         self.drive.set_break_mode(True)
-        self.drive.set_conversion_factor(.0485823156, 8.097052603e-4)
+        self.drive.set_conversion_factor(config.drive_pose_factor, config.drive_velocity_factor)
         self.drive.burn_flash()
 
         self.turn = TorqueNEO(ports.turnID)
-        self.turn.set_current_limit(25)
-        self.turn.set_voltage_compensation(12.6)
+        self.turn.set_current_limit(config.turn_max_current)
+        self.turn.set_voltage_compensation(config.voltage_compensation)
         self.turn.set_break_mode(True)
-        self.turn.set_conversion_factor(77.97432966, 1)
+        self.turn.set_conversion_factor(config.turn_gear_ratio * 2 * math.pi, 1)
         self.turn.burn_flash()
 
         self.encoder: CANCoder = CANCoder(ports.encoderID)
 
         self.offset = offset
 
-        self.drivePID = PIDController(.1, 0, 0)
-        self.turnPID = PIDController(.5, 0, 0)
+        self.drivePID = PIDController(config.drive_p, config.drive_i, config.drive_d)
+        self.turnPID = PIDController(config.turn_p, config.turn_i, config.turn_d)
         self.turnPID.enableContinuousInput(-math.pi, math.pi)
-        self.driveFF = SimpleMotorFeedforwardMeters(.1, .2)
+        self.driveFF = SimpleMotorFeedforwardMeters(config.drive_p, config.drive_ff)
 
         self.aggregate_position = SwerveModulePosition(0, Rotation2d.fromDegrees(0))
         self.last_sampled_time = -1
@@ -53,7 +84,7 @@ class TorqueSwerveModule2022:
 
             self.drive.set_percent(drivePIDOutput + driveFFOutput)
         else:
-            self.drive.set_percent(optimized.speed / 4.6)
+            self.drive.set_percent(optimized.speed / self.config.max_velocity)
 
         turnPIDOutput = self.turnPID.calculate(self.get_rotation().radians(), optimized.angle.radians())
         self.turn.set_percent(turnPIDOutput)
@@ -76,4 +107,4 @@ class TorqueSwerveModule2022:
         return SwerveModulePosition(self.drive.get_position(), self.get_rotation())
     
     def get_rotation(self) -> Rotation2d:
-        return coterminal(Rotation2d.fromDegrees((self.encoder.getPosition() - self.offset) * 180 / math.pi))
+        return coterminal(Rotation2d(self.encoder.getPosition() - self.offset))
