fb2f50ad8c0ef67f1dd3a4431a170cddaa43c10b
humandavey <davey.adams.three@gmail.com>
2024-04-01 09:31:25 -0500

    Update path following

--- auto/command/TorqueFollowPath.py
+++ auto/command/TorqueFollowPath.py
@@ -1,29 +1,33 @@
 from lib.auto.TorqueCommand import TorqueCommand
-from pathplannerlib.controller import PPRamseteController
+from pathplannerlib.controller import PPHolonomicDriveController
 from pathplannerlib.path import PathPlannerPath
 from pathplannerlib.path import PathPlannerTrajectory
+from pathplannerlib.controller import PIDConstants
 from wpimath.geometry import Pose2d
 from wpimath.geometry import Translation2d, Rotation2d
 from wpimath.kinematics import ChassisSpeeds
 from wpilib import DriverStation
-import systems, wpilib
+import systems, wpilib, math
 from pathplannerlib.telemetry import PPLibTelemetry
 
 class TorqueFollowPath(TorqueCommand):
-    def __init__(self) -> None:
+    def __init__(self, path_name: str) -> None:
         super().__init__()
         
+        self.path_name = path_name
         self.timer = wpilib.Timer()
+
+        self.controller = PPHolonomicDriveController(PIDConstants(10, 0, 0), PIDConstants(math.pi, 0, 0), 4.6, systems.drivebase.get_radius())
     
     def init(self) -> None:
-        path = PathPlannerPath.fromPathFile('path name') # path supplier
+        self.path = PathPlannerPath.fromPathFile(self.path_name)
 
         if DriverStation.getAlliance() == DriverStation.Alliance.kRed:
-            path = path.flipPath()
+            self.path = self.path.flipPath()
         
-        self.trajectory = path.getTrajectory(ChassisSpeeds(), Rotation2d.fromDegrees(systems.drivebase.get_gyro_measurement()))
+        self.trajectory = self.path.getTrajectory(ChassisSpeeds(), Rotation2d.fromDegrees(systems.drivebase.get_gyro_measurement()))
         
-        PPLibTelemetry.setCurrentPath(path)
+        PPLibTelemetry.setCurrentPath(self.path)
         
         startingPose = self.trajectory.getInitialDifferentialPose()
         systems.drivebase.set_pose(startingPose)
@@ -32,6 +36,10 @@ class TorqueFollowPath(TorqueCommand):
     def continuous(self) -> None:
         elapsed = self.timer.get()
         desired = self.trajectory.sample(elapsed)
+
+        speeds = self.controller.calculateRobotRelativeSpeeds(systems.drivebase.get_pose(), desired)
+
+        systems.drivebase.set_speeds(speeds.vx, speeds.vy, speeds.omega)
         
         PPLibTelemetry.setCurrentPose(systems.drivebase.get_pose())
         PPLibTelemetry.setTargetPose(desired.getDifferentialPose())
