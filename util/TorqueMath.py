850739a6d184c307c0582adbc2561edd6c0e3372
maazie2000 <fowlartemis34@gmail.com>
2024-04-25 17:15:54 -0500

    Added Poly. Regression

--- util/TorqueMath.py
+++ util/TorqueMath.py
@@ -1,8 +1,19 @@
 from wpimath.geometry import Rotation2d
 from wpimath import angleModulus
+import numpy as np
+
+class TorquePolynomialRegression:
+    def __init__(self, x, y) -> None:
+        self.model = np.polyfit(x, y, 2)
+        self.inference = np.poly1d(self.model)
+
+    def predict(self, x):
+        return self.inference(x)
+
 
 def scaled_linear_deadband(value: float, scale: float) -> float:
     return 0 if abs(value) < scale else (value - (abs(value) / value) * scale) / (1 - scale)
 
 def coterminal(rotation: Rotation2d) -> Rotation2d:
     return Rotation2d(angleModulus(rotation.radians()))
+
